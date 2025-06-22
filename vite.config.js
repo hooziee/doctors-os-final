7eaa896ce7b5497bd4bff17408f8939800b8b20a
Huseyin <huseyinturacli123@gmail.com>
2025-06-22 17:17:27 +0700

    Final config fixes for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This section fixes the "import.meta" error by telling Vite to build
-  // for a modern browser version that supports this feature.
-  build: {
-    target: 'es2020'
-  },
-  esbuild: {
-    target: 'es2020'
-  },
-  optimizeDeps: {
-      esbuildOptions: {
-          target: 'es2020',
-      },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 })
+
